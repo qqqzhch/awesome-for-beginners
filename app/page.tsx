9272b702a26a1bbb6284c84a0b40d49e5b64b193
qqqzhch <qqqzhch@163.com>
2023-05-20 09:48:42 +0800

    feat: add btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
+"use client"
 import Image from 'next/image'
 import data from '../data.json'
-import { use, useMemo } from 'react';
+import { use, useCallback, useMemo, useState } from 'react';
 
 type technologie={
   key:string,
   num:number
 }
 export default function Home() {
-  console.log('useMemo',useMemo)
-  useMemo(()=>{},[])
+  const [technologie,setTechnologie]=useState<string>()
+  const [search,setSearch]=useState<string>()
   const technologies = useMemo(()=>{
     let list:technologie[]=[]
     if(data==null){
@@ -28,7 +29,29 @@ export default function Home() {
 
     })
     return list
-   },[data])
+   },[])
+
+   const repositoriesList =useMemo(()=>{
+    if(technologie==undefined&&search==undefined){
+      return data.repositories 
+    }
+   const list = data.repositories.filter((item)=>{
+    let result;
+    if(search!==undefined&&search!==""){
+      result =(item.name.toLowerCase().includes(search.toLowerCase())||
+              item.technologies.includes(search.toLowerCase())||
+              item.description.toLowerCase().includes(search.toLowerCase()))
+    }
+    if(technologie!==undefined){
+      result=item.technologies.includes(technologie)||result
+    }
+    return result
+    
+   
+    })
+    return list
+
+   },[technologie,search])
 
   return (
    <>
@@ -73,22 +96,23 @@ export default function Home() {
  </section>
  <section className=' flex'>
   
-<form className=' w-full max-w-2xl  m-auto'>   
+{/* <div className=' w-full max-w-2xl  m-auto'>   
   <label for="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
   <div className="relative">
       <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
           <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
       </div>
-      <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required>
+      <input onChange={(e)=>{setSearch(e.currentTarget.value);console.log(e.currentTarget.value)}} type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required>
       </input>
-      <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+      <button  className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
   </div>
-</form>
+</div> */}
  </section>
  <section>
  <div className='flex  flex-row flex-wrap  my-10 justify-center '>
     {technologies.map((one,index)=>{
-      return (<button type="button" className="m-1 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+      return (<button key={index} onClick={()=>{setTechnologie(one.key)}} type="button" 
+      className={`m-1 text-blue-700 hover:text-white border ${technologie==one.key?"bg-blue-800 text-white":""} border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800`}>
         {one.key}
         <span className="inline-flex items-center justify-center   min-w-[1.5rem]  h-6 ml-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
         {one.num}
@@ -96,9 +120,9 @@ export default function Home() {
         </button>)
     })}
   </div>
-  {data.repositories.map((item)=>{
+  {repositoriesList.map((item,index)=>{
     return (
-      <div className="container flex flex-col w-full max-w-2xl p-6 mx-auto divide-y rounded-md divide-gray-700 dark:bg-gray-900 dark:text-gray-100">
+      <div key={index} className="container flex flex-col w-full max-w-2xl p-6 mx-auto divide-y rounded-md divide-gray-700 dark:bg-gray-900 dark:text-gray-100">
 	<div className="flex justify-between p-4">
 		<div className="flex space-x-4">
 			{/* <div>
@@ -110,9 +134,9 @@ export default function Home() {
 			</div>
 		</div>
 		<div className="flex items-center space-x-2 dark:text-yellow-500">
-			{item.technologies.map((tag)=>{
+			{item.technologies.map((tag,index)=>{
         return (
-          <span className="text-xl font-bold">{tag}</span>
+          <span key={index} className="text-xl font-bold">{tag}</span>
         )
 
       })}
