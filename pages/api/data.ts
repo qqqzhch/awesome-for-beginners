c63fc9b06464b744e97ec2f1b07de49f346b9df1
qqqzhch <qqqzhch@163.com>
2023-05-20 10:06:06 +0800

    Deploy

--- pages/api/data.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import data from '../../data.json' 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // const resdata = await fetch('https://raw.githubusercontent.com/MunGell/awesome-for-beginners/main/data.json');
-  // const data = await resdata.json()
-  res.status(200).json(data);
-}
