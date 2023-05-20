9ec5d7d968ac8916827d48149eed7c3fea86c2db
qqqzhch <qqqzhch@163.com>
2023-05-20 09:54:10 +0800

    feat: config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    basePath: '/awesome-for-beginners',
+    experimental: {
+      appDir: true,
+    },
+}
 
 module.exports = nextConfig
